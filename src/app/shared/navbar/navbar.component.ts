31f91d096f0dd50a22646d542220fc3bc4f6f3a0
Akshay Kedar <kedarakshay38@gmail.com>
2026-03-29 15:48:35 +0530

    Uodating app Routing and Checkout component

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { CartService } from '../../services/cart.service';
 import { User } from '../../models/user';
 
 @Component({
@@ -13,12 +14,20 @@ import { User } from '../../models/user';
 })
 export class NavbarComponent implements OnInit {
   user: User | null = null;
+  cartItemCount = 0;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
     this.authService.currentUser$.subscribe(user => {
       this.user = user;
+    });
+
+    this.cartService.cartItems$.subscribe(() => {
+      this.cartItemCount = this.cartService.getItemCount();
     });
   }
 
